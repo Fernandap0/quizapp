3e996e47cb755a4c41181b98f1c0594179e880c5
Fernanda Pereira Cavalcante - ADS A <fernandapri.cavalcante@gmail.com>
2024-05-21 17:26:54 +0000

    adicionada a função para escolher o tema do quiz no indez.html

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ const body = document.querySelector("body")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
-
 })
 
 verificarTema(body, botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html" 
+}
